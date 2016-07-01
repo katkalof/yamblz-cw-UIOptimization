def33f9d96034313813722abe471ab1557ca991c
Ilya Sergeev <i-sergeev@yandex-team.ru>
2016-07-01 12:33:28 +0300

    add UI and Model folders

---
+++ app/src/main/java/com/school/uioptimizationsample/SampleApplication.java
@@ -0,0 +1,10 @@
+package com.school.uioptimizationsample;
+
+import android.app.Application;
+
+/**
+ * Created by i-sergeev on 01.07.16
+ */
+public class SampleApplication extends Application
+{
+}
